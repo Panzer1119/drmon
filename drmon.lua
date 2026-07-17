3ca504a7c7a06c6a9a6f0733cd347e604148cbcf
Paul Hagedorn <panzer1119@gmail.com>
2026-07-17 05:00:59 +0200

    fix: formatting of percentages in progress bars
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- drmon.lua
+++ drmon.lua
@@ -221,7 +221,7 @@ function update()
     local satPercent
     satPercent = math.ceil(ri.energySaturation / ri.maxEnergySaturation * 10000)*.01
 
-    f.draw_text_lr(mon, 2, 11, 1, "Energy Saturation", satPercent .. "%", colors.white, colors.white, colors.black)
+    f.draw_text_lr(mon, 2, 11, 1, "Energy Saturation", string.format("%.2f%%", satPercent), colors.white, colors.white, colors.black)
     f.progress_bar(mon, 2, 12, mon.X-2, satPercent, 100, colors.blue, colors.gray)
 
     local fieldPercent, fieldColor
@@ -232,7 +232,7 @@ function update()
     if fieldPercent < 50 and fieldPercent > 30 then fieldColor = colors.orange end
 
     if autoInputGate == 1 then 
-      f.draw_text_lr(mon, 2, 14, 1, "Field Strength T:" .. targetStrength, fieldPercent .. "%", colors.white, fieldColor, colors.black)
+      f.draw_text_lr(mon, 2, 14, 1, "Field Strength T:" .. targetStrength, string.format("%.2f%%", fieldPercent), colors.white, fieldColor, colors.black)
     else
       f.draw_text_lr(mon, 2, 14, 1, "Field Strength", fieldPercent .. "%", colors.white, fieldColor, colors.black)
     end
@@ -247,7 +247,7 @@ function update()
     if fuelPercent >= 70 then fuelColor = colors.green end
     if fuelPercent < 70 and fuelPercent > 30 then fuelColor = colors.orange end
 
-    f.draw_text_lr(mon, 2, 17, 1, "Fuel ", fuelPercent .. "%", colors.white, fuelColor, colors.black)
+    f.draw_text_lr(mon, 2, 17, 1, "Fuel ", string.format("%.2f%%", fuelPercent), colors.white, fuelColor, colors.black)
     f.progress_bar(mon, 2, 18, mon.X-2, fuelPercent, 100, fuelColor, colors.gray)
 
     f.draw_text_lr(mon, 2, 19, 1, "Action ", action, colors.gray, colors.gray, colors.black)
